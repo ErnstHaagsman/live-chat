1fb54396710ae8dfc805a33d2a84fc55bb7d233f
Ernst Haagsman <e.haagsman@solpeo.com>
2014-07-10 15:28:49 +0200

    Client side websockets

--- public/js/client.js
+++ public/js/client.js
@@ -1,21 +1,29 @@
 function ChatController ($scope, $http){
 
-    $scope.messages = [
-        {
-            "nickname": "placeholder",
-            "text": "check angular"
-        },
-        {
-            "nickname": "placeholder",
-            "text": "message 2"
+    var sock = new SockJS('/live');
+
+    sock.onmessage = function(e){
+        data = JSON.parse(String(e.data));
+        if(Array.isArray(data)){
+            $scope.messages = data;
+        } else {
+            $scope.messages.push(data);
         }
-    ];
+
+        $scope.$apply();
+    };
+
+    $scope.messages = [];
 
     $scope.send_message = function(){
 
         // Don't send empty messages
         if($scope.message === "") return;
 
+        sock.send(JSON.stringify({
+            nickname: $scope.nickname,
+            text: $scope.message
+        }));
         console.log($scope.nickname + " sent message: " + $scope.message);
         $scope.message = "";
 
